9690268ce7ba1f27c3e89468af1295e048385fa9
setiyawt <setiyawati324@gmail.com>
2024-07-29 20:29:06 +0700

    quiz interface proses notification

---
+++ quiz_interface_proses_notification/go.mod
@@ -0,0 +1,3 @@
+module proses_notification
+
+go 1.22.5
---
+++ quiz_interface_proses_notification/main.go
@@ -0,0 +1,52 @@
+package main
+
+type notification interface {
+	importance() int
+}
+
+type directMessage struct {
+	senderUsername string
+	messageContent string
+	priorityLevel  int
+	isUrgent       bool
+}
+
+type groupMessage struct {
+	groupName      string
+	messageContent string
+	priorityLevel  int
+}
+
+type systemAlert struct {
+	alertCode      string
+	messageContent string
+}
+
+func (d directMessage) importance() int {
+	if d.isUrgent {
+		return 50
+	}
+	return d.priorityLevel
+}
+
+func (g groupMessage) importance() int {
+	return g.priorityLevel
+}
+
+func (s systemAlert) importance() int {
+	return 100
+}
+
+func processNotification(n notification) (string, int) {
+	switch v := n.(type) {
+	case directMessage:
+		return v.senderUsername, v.importance()
+	case groupMessage:
+		return v.groupName, v.importance()
+	case systemAlert:
+		return v.alertCode, v.importance()
+	default:
+		return "", 0
+
+	}
+}
