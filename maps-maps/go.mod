03dc9b22257e306cfcd211540d0bbfa4ac591c15
setiyawt <setiyawati324@gmail.com>
2024-08-09 14:42:16 +0700

    maps-maps

---
+++ maps-maps/go.mod
@@ -0,0 +1,3 @@
+module maps-maps
+
+go 1.22.6
---
+++ maps-maps/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "errors"
+
+func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
+	if len(names) != len(phoneNumbers) {
+		return nil, errors.New("invalid sizes")
+	}
+
+	userMap := make(map[string]user)
+	for i, name := range names {
+		userMap[name] = user{name, phoneNumbers[i]}
+	}
+
+	return userMap, nil
+}
+
+type user struct {
+	name        string
+	phoneNumber int
+}
