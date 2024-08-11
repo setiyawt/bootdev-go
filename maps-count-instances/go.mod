ac436e4832a1b30df355dc6dfc4c0eeb2663a4b3
setiyawt <setiyawati324@gmail.com>
2024-08-11 20:28:13 +0700

    maps-count-instances

---
+++ maps-count-instances/go.mod
@@ -0,0 +1,3 @@
+module maps-count-instances
+
+go 1.22.6
---
+++ maps-count-instances/main.go
@@ -0,0 +1,11 @@
+package main
+
+func getCounts(messagedUsers []string, validUsers map[string]int) {
+	for _, user := range messagedUsers {
+		if _, exists := validUsers[user]; exists {
+			validUsers[user]++
+		}
+
+	}
+
+}
